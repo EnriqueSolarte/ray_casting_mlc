cc014285268190c470379048f93117d56258bbcb
EnriqueSolarte <enrique.solarte.pardo@gmail.com>
2024-06-29 22:53:46 +0800

    pre-compute MLC and xyz for pseudo-labeling

--- ray_casting_mlc/__init__.py
+++ ray_casting_mlc/__init__.py
@@ -2,8 +2,8 @@
 import geometry_perception_utils.config_utils
 import os
 
-MLC_PP_ROOT = os.path.dirname(os.path.abspath(__file__))
-MLC_PP_CFG_DIR = os.path.join(MLC_PP_ROOT, 'config')
+RAY_CASTING_MLC_ROOT = os.path.dirname(os.path.abspath(__file__))
+RAY_CASTING_MLC_CFG_DIR = os.path.join(RAY_CASTING_MLC_ROOT, 'config')
 
-os.environ['MLC_PP_ROOT'] = MLC_PP_ROOT
-os.environ['MLC_PP_CFG_DIR'] = MLC_PP_CFG_DIR
+os.environ['RAY_CASTING_MLC_ROOT'] = RAY_CASTING_MLC_ROOT
+os.environ['RAY_CASTING_MLC_CFG_DIR'] = RAY_CASTING_MLC_CFG_DIR
---
+++ ray_casting_mlc/create_pseudo_labels/pre_compute_mlc.py
@@ -0,0 +1,70 @@
+from mlc import compute_pseudo_labels, draw_mlc_labels
+from geometry_perception_utils.config_utils import save_cfg
+from geometry_perception_utils.vispy_utils import plot_list_pcl
+import vslab_360_datasets as vslab
+from geometry_perception_utils.io_utils import create_directory
+import os
+import numpy as np
+from tqdm import tqdm
+from pathlib import Path
+import hydra
+import logging
+from geometry_perception_utils.io_utils import get_abs_path
+from layout_models import horizon_net_v2 as hn
+import ray_casting_mlc
+
+
+def create_and_save_mlc_labels(__list_ly, __ly, output_dir):
+    uv_ceiling_ps, uv_floor_ps, std_ceiling, std_floor, _ = compute_pseudo_labels(
+        list_frames=__list_ly, ref_frame=__ly)
+
+    # ! Saving pseudo labels
+    v_coord = np.vstack((uv_ceiling_ps[1], uv_floor_ps[1]))
+    std = np.vstack((std_ceiling, std_floor))
+    phi_bon = (v_coord / 512 - 0.5) * np.pi
+
+    label = np.vstack((phi_bon, std))
+    np.save(f"{output_dir}/label/{__ly.idx}", label)
+
+    draw_mlc_labels(ref=__ly,
+                    uv_ceiling_ps=uv_ceiling_ps,
+                    uv_floor_ps=uv_floor_ps,
+                    std_ceiling=std_ceiling,
+                    std_floor=std_floor,
+                    _output_dir=output_dir)
+
+
+@hydra.main(version_base=None,
+            config_path=get_abs_path(__file__),
+            config_name="cfg.yaml")
+def main(cfg):
+    logging.warning("Running script")
+    save_cfg(cfg, [__file__])
+
+    # load HN model
+    model = hn.WrapperHorizonNetV2(cfg.model)
+    hn.load_ckpt(cfg.model.ckpt, model)
+
+    dt = vslab.load_mvl_dataset(cfg.mvl_dataset)
+
+    # Create dir to save 360-MLC pseudo-labels
+    create_directory(f"{cfg.output_dir}/label", delete_prev=True)
+    create_directory(f"{cfg.output_dir}/vis", delete_prev=True)
+    output_dir = cfg.output_dir # expected path for 360-mlc
+    
+    for idx, scene in enumerate(dt.list_scenes):
+        logging.info(
+            f"Processing scene: {scene} - {idx}: {idx/dt.list_scenes.__len__()*100:.2f}%"
+        )
+
+        list_ly = dt.get_list_ly(scene_name=scene)
+        # Estimate ly in the list of layouts
+        hn.estimate_within_list_ly(model, list_ly)
+        
+        for ly in tqdm(list_ly, desc="Creating MLC pseudo-labels"):
+            create_and_save_mlc_labels(list_ly, ly, output_dir)
+    logging.warning("Finished script")
+
+
+if __name__ == '__main__':
+    main()
